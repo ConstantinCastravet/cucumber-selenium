f27065489a9aef72169aa105de3ad46994a33b13
jostea <jostea1488@gmail.com>
2019-11-29 00:43:04 +0200

    First commit

---
+++ src/main/features/scenario.feature
@@ -0,0 +1,7 @@
+Feature: Reset functionality on login page of Application
+
+  Scenario: Verification of Reset button
+    Given Open the Chrome
+    Then Enter specified name
+    Then  Click search button
+    Then Pictures must be view
---
+++ src/main/java/testRunner/Runner.java
@@ -0,0 +1,10 @@
+package testRunner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/main/features",glue = {"stepDefinition"})
+public class Runner {
+}
